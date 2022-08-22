fce1cbafd269102ea5e25893c4003c3b78da0153
netoudi <netoudi@rocketmail.com>
2022-08-22 11:25:30 -0300

    feat: add product validations

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './exception-filters/entity-not-found.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
   await app.listen(3333);
 }
+
 bootstrap();
